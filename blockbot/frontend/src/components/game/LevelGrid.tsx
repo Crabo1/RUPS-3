5615bd7279f9d514000a14008f9736b38a6722ae
miha.plibersek <miha.plibersek@student.um.si>
2026-02-05 16:13:56 +0100

    new icons for components in blockbot

--- blockbot/frontend/src/components/game/LevelGrid.tsx
+++ blockbot/frontend/src/components/game/LevelGrid.tsx
@@ -82,42 +82,42 @@ export function LevelGrid({
               )}
               {currentObj === 'bulb' && (
                 <img
-                  src="/circuit/components/lamp.svg"
+                  src="/circuit/components/imgs/lamp.svg"
                   alt="Bulb"
                   className="absolute inset-0 h-full w-full p-2"
                 />
               )}
               {currentObj === 'ammeter' && (
                 <img
-                  src="/circuit/components/ammeter.svg"
+                  src="/circuit/components/imgs/ammeter.svg"
                   alt="Ammeter"
                   className="absolute inset-0 h-full w-full p-2"
                 />
               )}
               {currentObj === 'battery' && (
                 <img
-                  src="/circuit/components/battery.svg"
+                  src="/circuit/components/imgs/battery.svg"
                   alt="Battery"
                   className="absolute inset-0 h-full w-full p-2"
                 />
               )}
               {currentObj === 'resistor' && (
                 <img
-                  src="/circuit/components/resistor.svg"
+                  src="/circuit/components/imgs/resistor.svg"
                   alt="Resistor"
                   className="absolute inset-0 h-full w-full p-2"
                 />
               )}
               {currentObj === 'switch' && (
                 <img
-                  src="/circuit/components/switch-off.svg"
+                  src="/circuit/components/imgs/switch.svg"
                   alt="Switch"
                   className="absolute inset-0 h-full w-full p-2"
                 />
               )}
               {currentObj === 'voltmeter' && (
                 <img
-                  src="/circuit/components/voltmeter.svg"
+                  src="/circuit/components/imgs/voltmeter.svg"
                   alt="Voltmeter"
                   className="absolute inset-0 h-full w-full p-2"
                 />
